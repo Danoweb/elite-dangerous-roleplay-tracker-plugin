41a7380b63b11fe51e19696079bdfc20b1bdcc79
Timothy Crory <tcrory@gmail.com>
2018-03-16 11:08:07 -0400

    Initial implementation of the GET HTTP requests. Need to update to return the proper objects after testing.

--- edrp.py
+++ edrp.py
@@ -11,10 +11,11 @@ import requests
 EDRP_API_URL = 'http://edrp-api.danowebstudios.com'
 
 
+# Low Level API Functions
 def post(payload):
     """
     Create an HTTP POST request for the EDRP API.
-    :param payload: Payload for the HTTP Post request.
+    :param payload: Payload for the HTTP POST request.
     :return:
     """
     r = requests.post('{}{}'.format(EDRP_API_URL, payload))
@@ -23,27 +24,43 @@ def post(payload):
     print('Response: {}'.format(r.text))
 
 
-def logon(cmdr):
+def get(payload):
     """
-    Send a logon HTTP request to the EDRP API.
+    Create an HTTP GET request for the EDRP API.
+    :param payload: Payload for the HTTP GET request.
+    :return: JSON object received from the EDRP API.
+    """
+    r = requests.get('{}{}'.format(EDRP_API_URL, payload))
+    print('Payload: {}'.format(payload))
+    print('Status Code: {}'.format(r.status_code))
+    print('Response: {}'.format(r.text))
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# High Level API Functions
+def post_logon(cmdr):
+    """
+    Set an event marker for a CMDR logging onto the plugin.
     :param cmdr: CMDR name.
     :return:
     """
     post('/logon/{}'.format(cmdr))
 
 
-def logoff(cmdr):
+def post_logoff(cmdr):
     """
-    Send a logoff HTTP request to the EDRP API.
+    Set an event marker for a CMDR logging off the plugin.
     :param cmder: CMDR name.
     :return:
     """
     post('/logoff/{}'.format(cmdr))
 
 
-def station(station, cmdr):
+def post_station(station, cmdr):
     """
-    Send a station HTTP request to the EDRP API.
+    Set an event marker for a CMDR entering a station.
     :param station: Station name.
     :param cmdr: CMDR name.
     :return:
@@ -51,11 +68,37 @@ def station(station, cmdr):
     post('/station/{}/{}'.format(station, cmdr))
 
 
-def system(system, cmdr):
+def post_system(system, cmdr):
     """
-    Send a system HTTP request to the EDRP API.
+    Set an event marker for a CMDR entering a star system.
     :param system: System name.
     :param cmdr: CMDR name.
     :return:
     """
     post('/system/{}/{}'.format(system, cmdr))
+
+
+def get_active():
+    """
+    Get a list of users with an event from the plugin within the last 10
+    minutes.
+    :return: List of user names.
+    """
+    # TODO: Once the format for the returned object is discovered, update
+    #       this to check for list content and then return the actual list
+    #       if it is present.
+    active_json = get('/active')
+    return active_json
+
+
+def get_active_count():
+    """
+    Get a count of the users with an event from the plugin within the last 10
+    minutes.
+    :return: User count.
+    """
+    # TODO: Once the format for the returned object is discovered, update
+    #       this to check for list content and then return the actual list
+    #       if it is present.
+    active_count_json = get('/active-count')
+    return active_count_json
